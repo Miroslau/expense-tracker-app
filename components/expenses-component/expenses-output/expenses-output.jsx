c34b6ba7b4875f1e2091bf586c02721a22a1d149
Miroslav <mira2408@mail.ru>
2022-07-06 12:15:57 +0300

    feat: feature/142_Managing_app-wide_state_with_context

--- components/expenses-component/expenses-output/expenses-output.jsx
+++ components/expenses-component/expenses-output/expenses-output.jsx
@@ -4,63 +4,6 @@ import ExpensesSummary from "../expenses-summary/expenses-summary";
 import ExpensesList from "../expenses-list/expenses-list";
 import { GlobalStyles } from "../../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.99,
-    date: new Date("2022-01-05"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas",
-    amount: 5.99,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2021-02-19"),
-  },
-  {
-    id: "e5",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2021-02-18"),
-  },
-  {
-    id: "e6",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2021-02-18"),
-  },
-  {
-    id: "e7",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2021-02-18"),
-  },
-  {
-    id: "e8",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2021-02-18"),
-  },
-  {
-    id: "e9",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2021-02-18"),
-  },
-];
-
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View style={styles.container}>
